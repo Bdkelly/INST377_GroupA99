cc8fd601dee43eccbf2444aa47a5c5c8472a4cff
bdkelly <benjaminkelly97@gmail.com>
2020-11-12 12:42:44 -0500

    movedmapped

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('src'));
+app.use(express.static('websrc'));
 
 app.route('/api')
   .get((req, res) => {
