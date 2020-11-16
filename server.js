f326a1df5d3e372966e70fd104a9fca3b129c141
bdkelly <benjaminkelly97@gmail.com>
2020-11-16 11:15:32 -0500

    w

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
