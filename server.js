654d925a79f7018b667d8521f1403f81c9dafd2a
bdkelly <benjaminkelly97@gmail.com>
2020-11-30 22:04:45 -0500

    port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import sqlite3 from 'sqlite3';
 dotenv.config();
 //
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 8080;
 //
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
